472ee1371e0469b276c6e27049183d9defb8f331
shoneyang0718 <d1349803@o365.fcu.edu.tw>
2026-06-04 22:52:54 +0800

    Fix asset loading by using relative base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/fuel-pulse-cpi-vue-project/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
