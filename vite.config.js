4002002918ba1da8b1e8728e18eff7e3af87884f
shoneyang0718 <d1349803@o365.fcu.edu.tw>
2026-06-04 22:49:08 +0800

    Add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/fuel-pulse-cpi-vue-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
